81775eeaab37110d0d6d77c9480395a1af4d9950
miaravnanger <mia_maroen@hotmail.com>
2025-12-17 12:44:10 +0100

    auth middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+export function requireAuth(req, res, next) {
+  const authHeader = req.get("authorization");
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Missing Authorization header" });
+  }
+
+  const parts = authHeader.split(" ");
+  if (parts.length !== 2 || parts[0] !== "Bearer") {
+    return res
+      .status(401)
+      .json({ error: "Invalid Authorization header format" });
+  }
+
+  const token = parts[1];
+	let payload;
+
+  try {
+		payload = jwt.verify(token, JWT_SECRET);
+    const { sid } = payload;
+    if (!isSessionActive(sid)) {
+      return res.status(401).json({ error: "Session expired" });
+    }
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
+
+  req.user = payload;
+  next();
+}
